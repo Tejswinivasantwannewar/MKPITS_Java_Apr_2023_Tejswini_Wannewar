bb3e6af1fbd14039c89c99c0c2dac4054c2056c4
Tejswinivasantwannewar <tejswiniwannewar1212@gmail.com>
2023-04-17 14:04:07 +0530

    delet

---
+++ C_program/AREAOFRE.C
@@ -0,0 +1,18 @@
+
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+
+int l,w, area;
+
+
+printf("area of rectangle:");      //formula=l*w
+scanf("%d %d",&l,&w);
+
+area=l*w;
+
+printf("the area of rectangle=%d",area);
+
+getch();
+}
