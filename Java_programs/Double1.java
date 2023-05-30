ae0adf7f3e9faf0195907067a5bd03d371f7f2c1
Tejswinivasantwannewar <tejswiniwannewar1212@gmail.com>
2023-05-30 10:58:19 +0530

    30th may

---
+++ Java_programs/Double1.java
@@ -0,0 +1,21 @@
+class Double
+{
+	public long addDouble(long num1,long num2)
+{
+	return num1+num2;
+}
+}
+   
+   
+class Double1
+{
+   public static void main(String[] agrs)
+{
+   Double double1=new Double();
+   double ans=double1.addDouble (231,450);
+   System.out.println(ans);
+}
+}
+ 
+   
+ 
