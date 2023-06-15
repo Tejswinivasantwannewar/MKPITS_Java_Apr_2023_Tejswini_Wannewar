b389a68a40cf062aa1c040fd77cc62ca49e17335
Tejswinivasantwannewar <tejswiniwannewar1212@gmail.com>
2023-06-15 23:19:25 +0530

    entertainment real life inheritance

---
+++ Java_programs/Entertainment.java
@@ -0,0 +1,15 @@
+
+class Entertainment{
+List <Pattern> patterns;
+Integer no_Gadgets;
+String language;
+String quality;
+String advertisement;
+String crime;
+String comedy;
+EntertainmentType entertainmentType;
+}
+
+
+
+
