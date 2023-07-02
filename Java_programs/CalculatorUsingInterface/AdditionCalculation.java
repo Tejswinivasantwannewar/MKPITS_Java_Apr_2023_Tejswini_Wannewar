263ddf43988c4ca40fb20f9809dafe726da390db
Tejswinivasantwannewar <tejswiniwannewar1212@gmail.com>
2023-07-02 22:58:58 +0530

    calculator Using Interface

---
+++ Java_programs/CalculatorUsingInterface/AdditionCalculation.java
@@ -0,0 +1,10 @@
+public class AdditionCalculation implements CalculateValue{
+    public double getCalculatedValue(double operand1, double operand2){
+        double addition = operand1 + operand2;
+        return addition;
+    }
+}
+
+
+
+
