4b7347135ac1a1ac24918fac20b403a68517609e
Tejswinivasantwannewar <tejswiniwannewar1212@gmail.com>
2023-07-03 23:09:42 +0530

    Array addition of every fourth number and square of that number using interface in java

---
+++ Java_programs/ArrayUsingInterface/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+public static void main(String args[]){
+    SumOfEveryFourthNumber sumOfEveryFourthNumber=new SumOfEveryFourthNumber();
+    sumOfEveryFourthNumber.calculatesquareOfAddition();
+}
+
+
+    }
+
+
+
