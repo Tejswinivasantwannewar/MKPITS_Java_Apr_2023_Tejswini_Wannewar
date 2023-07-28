129e5da2b2562dd54b91e6716ed17eed7f9be432
Tejswinivasantwannewar <tejswiniwannewar1212@gmail.com>
2023-07-28 11:06:24 +0530

    javacript tasks

---
+++ javascrpt/online task/animation.js
@@ -0,0 +1,11 @@
+  //  Side Bar push content in left directon 
+
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("main").style.marginLeft= "0";
+}
