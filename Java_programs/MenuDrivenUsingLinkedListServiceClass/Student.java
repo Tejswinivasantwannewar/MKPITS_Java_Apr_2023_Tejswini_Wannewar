7ba096038b3c80f5f70f84b69948691664c6a0f0
Tejswinivasantwannewar <tejswiniwannewar1212@gmail.com>
2023-08-29 16:04:30 +0530

    menu Driven Program using linked list methods

---
+++ Java_programs/MenuDrivenUsingLinkedListServiceClass/Student.java
@@ -0,0 +1,20 @@
+public class Student {
+
+        private String name;
+        private  int marks;
+
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public void setMarks(int marks) {
+            this.marks = marks;
+        }
+
+        @Override
+        public String toString() {
+            return "Student " + "name= " + name + '\t' + "marks= " + marks ;
+        }
+    }
+
