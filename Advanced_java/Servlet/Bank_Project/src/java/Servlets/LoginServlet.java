70753e8914465e9d0802213e8150fab02dd47a1d
Tejswinivasantwannewar <tejswiniwannewar1212@gmail.com>
2023-10-27 21:45:44 +0530

    Banking Application

---
+++ Advanced_java/Servlet/Bank_Project/src/java/Servlets/LoginServlet.java
@@ -0,0 +1,141 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import static java.lang.System.out;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author tejswini
+ */
+public class LoginServlet extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    String UserId;
+    String PassWord;
+    
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException, ClassNotFoundException, SQLException,Exception {
+        response.setContentType("text/html;charset=UTF-8");
+        String id=request.getParameter("user_id");
+        String pass=request.getParameter("password");
+        
+        try (PrintWriter out = response.getWriter()) {
+             Class.forName("com.mysql.cj.jdbc.Driver");
+            out.println("driver load");
+            
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/BankDeatails", "root", "Teju@123");
+            out.println("Establish connection");
+            
+          PreparedStatement preparedStatement = connection.prepareStatement("select * from  CustomerDeatails where User_id=? and UserPassword=?");
+            preparedStatement.setString(1, id);
+            preparedStatement.setString(2, pass);
+          
+            ResultSet resultset=preparedStatement.executeQuery();
+            if(resultset.next()){
+                out.println("<h2><center>Login successful</center></h2>");
+                
+                RequestDispatcher requestDispatcher= request.getRequestDispatcher("DisplayMsg.html");
+                requestDispatcher.forward(request,response);
+                
+            }else {
+                out.println("<h2><center>Incorrect User_id Password</center></h2>");
+                out.println("<a href='index.html'><center><h1>Login Page</h1></center></a>");
+                
+            }
+            }
+            catch(Exception e)
+            {
+            out.println(e);
+
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet LoginServlet</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet LoginServlet at " + request.getContextPath() + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+            
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try {
+            processRequest(request, response);
+        } catch (SQLException ex) {
+            Logger.getLogger(LoginServlet.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (Exception ex) {
+            Logger.getLogger(LoginServlet.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try {
+            processRequest(request, response);
+        } catch (SQLException ex) {
+            Logger.getLogger(LoginServlet.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (Exception ex) {
+            Logger.getLogger(LoginServlet.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
