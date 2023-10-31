ac6c4984b7b40e850d79f3e5e6631c52dd4e4145
Tejswinivasantwannewar <tejswiniwannewar1212@gmail.com>
2023-10-31 16:30:59 +0530

    banking application

---
+++ Database_Mysql/BankingApplicationMVCDatabase.sql
@@ -0,0 +1,9 @@
+create database BankDeatailsMVC
+use BankDeatailsMVC
+
+
+create table BankDeatailsMVC(User_id varchar(255), UserPassword varchar(25),Customer_Name varchar(255),Customer_Address varchar(255),Customer_City varchar(255),
+							  Customer_Mobile varchar(255),Customer_Balance int)
+						
+     select * from BankDeatailsMVC               
+     
---
+++ Database_Mysql/StdentDetailsTable.sql
@@ -0,0 +1,4 @@
+create database  StudentDetail
+ use StudentDetail
+ create table StudentInfo(Roll_no varchar(20),Student_name varchar(20),Address varchar(80),City varchar(30));
+ 
