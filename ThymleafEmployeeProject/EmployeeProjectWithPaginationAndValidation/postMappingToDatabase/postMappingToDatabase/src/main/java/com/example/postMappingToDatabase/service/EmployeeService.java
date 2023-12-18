58d741151aed6af1b05d02031bb5cd38e4b60917
Tejswinivasantwannewar <tejswiniwannewar1212@gmail.com>
2023-12-18 10:48:25 +0530

    employee management thymleaf validation

---
+++ ThymleafEmployeeProject/EmployeeProjectWithPaginationAndValidation/postMappingToDatabase/postMappingToDatabase/src/main/java/com/example/postMappingToDatabase/service/EmployeeService.java
@@ -0,0 +1,17 @@
+package com.example.postMappingToDatabase.service;
+
+import com.example.postMappingToDatabase.model.Employee;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+
+public interface EmployeeService {
+//    List<Employee> getAllEmployee();
+  Page<Employee> getAllEmployee(Pageable pageable);
+    void save(Employee employee);
+    Employee updatebyid(Integer emp_id);
+    void deleteempdatabyId(Integer emp_id);
+
+
+}
---
+++ ThymleafEmployeeProject/EmployeeProjectWithPaginationAndValidation/postMappingToDatabase/postMappingToDatabase/src/main/java/com/example/postMappingToDatabase/service/EmployeeServiceimpl.java
@@ -0,0 +1,64 @@
+package com.example.postMappingToDatabase.service;
+
+
+
+import com.example.postMappingToDatabase.dao.EmployeeRepository;
+import com.example.postMappingToDatabase.model.Employee;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class EmployeeServiceimpl implements EmployeeService {
+
+    private EmployeeRepository employeeRepository;
+
+    @Autowired
+    public EmployeeServiceimpl(EmployeeRepository studentRepository) {
+
+        this.employeeRepository = studentRepository;
+    }
+
+//    @Override
+//    public List<Employee> getAllEmployee() {
+//
+//        return employeeRepository.findAll();
+//    }
+@Override
+public Page<Employee> getAllEmployee(Pageable pageable) {
+    return employeeRepository.findAll(pageable);
+}
+
+    @Override
+    public void save(Employee employee) {
+        employeeRepository.save(employee);
+    }
+
+    @Override
+    public Employee updatebyid(Integer emp_id) {
+        Optional<Employee> optional = employeeRepository.findById(emp_id);
+        Employee employee = null;
+        if (optional.isPresent())
+            employee = optional.get();
+        else
+            throw new RuntimeException(
+                    "Employee not found for id : " + emp_id);
+        return employee;
+    }
+
+
+    @Override
+
+    public void deleteempdatabyId(Integer emp_id)
+    {
+        employeeRepository.deleteById(emp_id);
+    }
+
+}
+
+
