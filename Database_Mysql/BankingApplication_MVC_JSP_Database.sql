c6bd042e4fd7c1fb10233c707dc451c26f14c5a1
Tejswinivasantwannewar <tejswiniwannewar1212@gmail.com>
2023-12-25 12:59:02 +0530

    mvc banking application

--- Database_Mysql/BankingApplication_MVC_JSP_Database.sql
+++ Database_Mysql/BankingApplication_MVC_JSP_Database.sql
@@ -10,3 +10,13 @@ create table Transactions_MVC_JSP(User_Id varchar(20),Transaction_date date,Amou
 foreign key (User_Id ) references BankDeatailsMVC_JSP_Table(User_id))
 
 select * from Transactions_MVC_JSP
+
+
+
+
+create table Admin(user_id varchar(25),User_Password varchar(100))
+select * from Admin
+
+create table Log(logTime timestamp ,Task varchar(200),user_id  varchar(29))
+select * from Log
+
